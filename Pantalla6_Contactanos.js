03c8f38430393c97665f835e5ce4df44d806b3d0
Lucero Estrada <lucero.perezestr@uanl.edu.mx>
2023-11-06 07:32:56 -0600

    Actualizacion de JavaScript

--- Pantalla6_Contactanos.js
+++ Pantalla6_Contactanos.js
@@ -1,72 +1,40 @@
-console.log("¡Mi archivo JavaScript está funcionando!");
+function SoloLetras(e){
+    
+  key = e.keyCode || e.which;
+  tecla = String.fromCharCode(key).toString();
+  letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyzÁÉÍÓÚáéíóú";
 
-document.addEventListener("DOMContentLoaded", function () {
-  const formulario = document.getElementById("miFormulario");
+  especiales = [8,13,32];
+  tecla_especial = false
+      for(var i in especiales) {
+          if (key == especiales[i]) {
+              tecla_especial = true;
+              break;
+          }
+      }
 
-  formulario.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    if (validarFormulario()) {
-      mostrarMensajeAgradecimiento();
-      redirigirANuevaPagina();
-    } else {
-      alert("Los datos son incorrectos. Por favor, revíselos y vuelva a intentarlo.");
-    }
-  });
-
-  function validarFormulario() {
-    const nombreInput = document.getElementById("Nombre");
-    const emailInput = document.getElementsByName("email")[0];
-    const telefonoInput = document.getElementById("TELEFONO");
-    const estadoSelect = document.getElementById("select-estado");
-    const mensajeTextarea = document.getElementById("MENSAJE");
-    const regexSoloLetras = /^[A-Za-z\s]+$/;
-
-    if (nombreInput.value.trim() === "" || !regexSoloLetras.test(nombreInput.value)) {
-      alert("El campo Nombre debe contener solo letras mayúsculas y minúsculas.");
-      return false;
-    }
-
-    if (emailInput.value.trim() === "" || !esCorreoValido(emailInput.value)) {
-      alert("Ingresa un correo electrónico válido.");
-      return false;
-    }
-
-    if (telefonoInput.value.trim() === "" || !esTelefonoValido(telefonoInput.value)) {
-      alert("Ingresa un número de teléfono válido de 10 dígitos.");
-      return false;
-    }
-
-    if (estadoSelect.value.trim() === "--Selecciona un estado--") {
-      alert("Selecciona un estado.");
-      return false;
-    }
-
-    if (mensajeTextarea.value.trim() === "") {
-      alert("El campo Mensaje es obligatorio.");
-      return false;
-    }
-
-    return true;
+  if(letras.indexOf(tecla) == -1 && !tecla_especial) {
+  alert("Favor de ingresar solamente letras.");
+  return false;
   }
 
-  function esCorreoValido(correo) {
-    const regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regexCorreo.test(correo);
+}
+
+function SoloNumeros(evt){
+
+  if(window.Event){
+      keynum = evt.keyCode;
+  }
+  else{
+      keymun = evt.which;
   }
 
-  function esTelefonoValido(telefono) {
-    const regexTelefono = /^\d{10}$/;
-    return regexTelefono.test(telefono);
+  if((keynum > 47 && keynum < 58) || keynum == 8 || keynum == 13){
+      return true;
   }
-
-  function mostrarMensajeAgradecimiento() {
-    alert("¡Gracias por enviar el formulario! Hemos recibido tus datos.");
+  else{
+      alert("Favor de ingresar solamente números.");
+      return false;
   }
-
-  function redirigirANuevaPagina() {
-    // Redirige a una nueva página HTML
-    window.location.href = "Pantalla1_CreacionUsuario.html";
-  }
-});
+}
 
